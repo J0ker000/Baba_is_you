879adfe480a8545d00dc967aa02671af6cbb5418
Denis <Denis.kulikov2006@mail.ru>
2024-02-26 20:55:47 +0300

    Make basic template.

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pygame
+import sys
+
 def main():
-    pass
+    width = 700     # Ширина окна
+    height = 500    # Высота окна
+    pygame.init()
+    FPS = 30
+    clock =pygame.time.Clock()
+    screen = pygame.display.set_mode([width, height])
+    gameover = False
+    while not gameover:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameover = True
+
+        screen.fill('white')
+        pygame.display.flip()
+        clock.tick(FPS)
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
