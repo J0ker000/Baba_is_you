31200aa301a333c1c7a1103f30e7b4893296e742
Denis <Denis.kulikov2006@mail.ru>
2024-03-05 22:12:48 +0300

    Add stop protocol and push protocol

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ import pygame
 import sys
 
 class Object:
-    def __init__(self, image, coords):
+    def __init__(self, image, coords, stop, push):
         self.image = image
         self.coords = coords
+        self.stop = stop
+        self.push = push
 def load_texture():
     floore = pygame.image.load('Textures/Floore.png')
     objects = {
-        'Baba': Object(pygame.image.load('Textures/Baba.png'), [[7, 7], [1, 2]])
+        'Baba': Object(pygame.image.load('Textures/Baba.png'), [[7, 7], [1, 2]], False, False),
+        'Wall': Object(pygame.image.load('Textures/Wall.png'), [[10, 10], [7, 8]], False, True)
     }
     return floore, objects
+def push_protocol(object_coords, shift, coords):
+    for image_coords in object_coords:
+        if image_coords == coords:
+            image_coords[0] += shift[0]
+            image_coords[1] += shift[1]
+    for i in range(len(object_coords)):
+        for coords in object_coords:
+            if object_coords.count(coords) >= 2:
+                coords[0] += shift[0]
+                coords[1] += shift[1]
+    return object_coords
+def stop_protocol(coords, shift):
+        coords[0] -= shift[0]
+        coords[1] -= shift[1]
 def render(screen, floore, objects):
     #Отрисовка пола грового поля
     for x in range(15):
@@ -26,23 +43,31 @@ def render(screen, floore, objects):
     pygame.display.flip()
 def management(key):
     if (key == pygame.K_w) or (key == pygame.K_UP):
-        return (0, -1)
+        return [0, -1]
     if (key == pygame.K_s) or (key == pygame.K_DOWN):
-        return (0, 1)
+        return [0, 1]
     if (key == pygame.K_a) or (key == pygame.K_LEFT):
-        return (-1, 0)
+        return [-1, 0]
     if (key == pygame.K_d) or (key == pygame.K_RIGHT):
-        return (1, 0)
-    return (0, 0)
-def move(object_coords, shift):
+        return [1, 0]
+    return [0, 0]
+def move(object_coords, shift, objects):
     for coords in object_coords:
         coords[0] += shift[0]
         coords[1] += shift[1]
         #Коллизия объектов с границами карты
-        if max(coords) > 14:
-            coords[coords.index(max(coords))] = 14
-        if min(coords) < 0:
-            coords[coords.index(min(coords))] = 0
+        if (max(coords) > 14) or (min(coords) < 0):
+            stop_protocol(coords, shift)
+
+        #Взаимодействие с объектами
+        for index in objects:
+            if objects[index].stop:
+                if coords in objects[index].coords:
+                    stop_protocol(coords, shift)
+
+            if objects[index].push:
+                objects[index].coords = push_protocol(objects[index].coords, shift, coords)
+
 def main():
     floore, objects = load_texture()
     width = floore.get_width()*15     # Ширина окна
@@ -60,7 +85,7 @@ def main():
                 gameover = True
             #Управление
             if event.type == pygame.KEYUP:
-                move(objects['Baba'].coords, management(event.key))
+                move(objects['Baba'].coords, management(event.key), objects)
 
         render(screen, floore, objects)
         clock.tick(FPS)
